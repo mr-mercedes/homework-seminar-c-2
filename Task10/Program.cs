96323b18f62fb5e04e0448495d90346f845117dc
Ekyd <Ekyd777@gmail.com>
2022-04-17 11:33:50 +0300

    add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Задача №10:Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+Console.WriteLine("Введите трехзначное число :");
+string? number = Console.ReadLine();
+Console.WriteLine(number[1]);
