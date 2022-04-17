8b5e7e9582022789f8d829ef6bd6bd18374a3796
Ekyd <Ekyd777@gmail.com>
2022-04-17 14:04:04 +0300

    add taskAdd without hardmode

---
+++ TaskAdd/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Задача дополнительная: Игра угадайка.");
+Console.WriteLine("Инструкция: Программа загадывает случайное число. Пользователь его угадывает. Если пользователь дает неправильный ответ, то программа сообщает, больше загаданное число или меньше ");
+int secretNumber = RandomNumber(1, 1000);
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+if (userNumber > secretNumber)
+{
+    Console.WriteLine($" Число {userNumber} больше загадонного числа");
+}
+else
+{
+    Console.WriteLine($" Число {userNumber} меньше загадонного числа");
+}
+
+int RandomNumber(int number1, int number2)
+{
+    return new Random().Next(number1, number2);
+}
