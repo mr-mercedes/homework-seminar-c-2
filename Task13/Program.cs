8aa25c88d5c535f2ab77803b58618a87b11d61e8
Ekyd <Ekyd777@gmail.com>
2022-04-17 13:05:46 +0300

    add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Задача №13:Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+Console.WriteLine("Введите число :");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 99;
+if (number > index)
+{
+    string stringNumber = Convert.ToString(number);
+    Console.WriteLine(stringNumber[2]);
+}
+else 
+{
+    Console.WriteLine("Третьего числа нет");
+}
