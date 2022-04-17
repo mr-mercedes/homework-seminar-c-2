813cd454783fea6ab16de4d29fb717d885660229
Ekyd <Ekyd777@gmail.com>
2022-04-17 14:43:46 +0300

    add taskAdd hardmode

--- TaskAdd/Program.cs
+++ TaskAdd/Program.cs
@@ -1,16 +1,36 @@
 ﻿Console.WriteLine("Задача дополнительная: Игра угадайка.");
 Console.WriteLine("Инструкция: Программа загадывает случайное число. Пользователь его угадывает. Если пользователь дает неправильный ответ, то программа сообщает, больше загаданное число или меньше ");
 int secretNumber = RandomNumber(1, 1000);
-Console.WriteLine("Введите число: ");
-int userNumber = Convert.ToInt32(Console.ReadLine());
-if (userNumber > secretNumber)
+int turn = 0;
+while(turn <= 2)
 {
-    Console.WriteLine($" Число {userNumber} больше загадонного числа");
+    Console.Write("Введите число: ");
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    if (userNumber == secretNumber)
+    {
+        Console.WriteLine($"Ура! Вы побидили Загаданное число {secretNumber}");
+        break;
+    }
+    if (userNumber > secretNumber)
+    {
+        Console.WriteLine($"Число {userNumber} больше загадонного числа");
+        turn++;
+        if(turn == 3)
+        {
+           Console.WriteLine($"Вы проиграли, загаданное число {secretNumber}"); 
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Число {userNumber} меньше загадонного числа");
+        turn++;
+         if(turn == 3)
+        {
+           Console.WriteLine($"Вы проиграли, загаданное число {secretNumber}"); 
+        }
+    }
 }
-else
-{
-    Console.WriteLine($" Число {userNumber} меньше загадонного числа");
-}
+
 
 int RandomNumber(int number1, int number2)
 {
